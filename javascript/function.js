e64d4ff6b13160b647ab5e04a8091c92154484f6
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-02-09 16:47:07 +0530

    Learnt the Functions and the arrays

---
+++ javascript/function.js
@@ -0,0 +1,21 @@
+// function Name(a, b, c) {
+//     console.log(a + b + c)
+// }
+// let c = 6;
+// Name(2, 3, c) 
+
+
+
+// Normal Function
+// let sum = function(a, b) {
+//     return a + b;
+// }
+
+// console.log(sum(2, 3))
+
+
+// Arrow Function
+
+let sum = (a, b) => a + b;
+
+console.log(sum(2, 7))
