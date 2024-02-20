0fff114a3371edd4a15430b69dc3d2615681685d
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-02-20 23:19:07 +0530

    Adding the funtions and arrays files

--- javascript/app.js
+++ javascript/app.js
@@ -1,4 +1,61 @@
-const a = 12;
-const b = 23;
+// // Variables
 
-console.log(a + b);
+// const a = 12;
+// console.log()
+
+// let age = 10;
+
+
+// // Conditionals in JS
+
+// if (age >= 18) {
+//     console.log("you are elegible")
+// } else {
+//     console.log("you are not elegible")
+// }
+
+
+// // switch 
+
+// let ch = 3;
+
+// switch (ch) {
+//     case 1:
+//         console.log("I am 1");
+//         break;
+//     case 2:
+//         console.log("I am two")
+//         break;
+//     case 3:
+//         console.log("I am Three")
+//         break;
+//     default:
+//         console.log("I am Default");
+// }
+
+
+
+// Loops in JS
+
+// for (let i = 1; i <= 50; i++) {
+//     console.log(i)
+// }
+
+
+
+// let i = 1;
+
+// while (i <= 10) {
+//     console.log(i)
+//     i++;
+// }
+
+
+// do while
+
+
+// let i = 11;
+// do {
+//     console.log(i)
+//     i++;
+// } while (i <= 10);
--- javascript/function.js
+++ javascript/function.js
@@ -16,6 +16,35 @@
 
 // Arrow Function
 
-let sum = (a, b) => a + b;
+// let sum = (a, b) => a + b;
 
-console.log(sum(2, 7))
+// console.log(sum(2, 7))
+
+
+// function funcName(a, b, c) {
+//     console.log(a + b + c)
+// }
+// let c = 5;
+
+
+// funcName(2, 3, c)
+
+
+
+// Types of Functions.....
+
+let sum = function(a, b) {
+
+    return a + b;
+
+}
+
+
+console.log(sum(3, 5))
+
+
+
+let summ = (a, b) => a + b;
+
+
+console.log(sum(3, 5))
